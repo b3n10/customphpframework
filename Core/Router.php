de2a39441da93708e2e083f89fea295afb142c9f
b3n10 <francisben.lleve@gmail.com>
2018-07-06 17:52:44 +0800

    Add custom routers, create methods to match regex with url

--- Core/Router.php
+++ Core/Router.php
@@ -4,13 +4,63 @@ namespace Core;
 
 class Router {
 
+	// save all routes in an array
+	private static $routes = [];
+
+	// save all params when searching for a match (self::$match($url))
+	private static $params = [];
+
+	// add routes
 	public static function add($route, $params = []) {
-		$route = preg_replace("/\/$/", "", $route);
-		echo $route;
+
+		// replace all / with \/
+		$route = preg_replace('/\//', '\/', $route);
+
+		// replace {word} with regex (?P<word>[a-z-]+)
+		$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+		// replace {id:\d+} with regex (?P<id>\d+)
+		$route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+		// include start and end regex search
+		$route = '/^' . $route . '$/i';
+
+		self::$routes[$route] = $params;
 	}
 
 	public static function match($url) {
-		echo $url;
+
+		// remove very last / if any from $url
+		$url = preg_replace('/\/$/', '', $url);
+
+		// look in $routes array and see if $url matches regex of $route
+		foreach (self::$routes as $route => $param) {
+
+			if (preg_match($route, $url, $matches)) {
+
+				// if match found, save array to $params
+				// e.g.
+				// [controller]	=> url0
+				// [action]			=> url1
+
+				foreach ($matches as $key => $value) {
+					if (is_string($key)) $param[$key] = $value;
+				}
+
+				self::$params = $param;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static function getRoutes() {
+		return self::$routes;
+	}
+
+	public static function getParams() {
+		return self::$params;
 	}
 
 }
--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
+// include composer's autoloader
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use \Core\Router;
 
 $url = $_SERVER['QUERY_STRING'];
 
-Router::add($url);
+Router::add('{controller}');
+Router::add('{controller}/{action}');
+Router::add('{controller}/{id:\d+}/{action}');
+
