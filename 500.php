6c5dda6ef864a0c31ba4e9ec4fd3de2a694da298
b3n10 <francisben.lleve@gmail.com>
2018-07-16 20:59:35 +0800

    Remove unnecessary file

--- 500.php
+++
@@ -1,5 +0,0 @@
-<?php require_once dirname(__DIR__) . "/header.php"; ?>
-	<h1>
-		500 internal server error. Sorry for the inconvenience.
-	</h1>
-<?php require_once dirname(__DIR__) . "/footer.php"; ?>
